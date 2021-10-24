15243f3834161ee9acb82e574c9dde1555cbfa03
Epkkc <71571866+Epkkc@users.noreply.github.com>
2021-10-24 10:57:53 +0300

    first commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,20 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("Сравнение бинов HelloWorld");
+        System.out.println("Сылка на первый бин = " + bean1);
+        System.out.println("Сылка на второй бин = " + bean2);
+        System.out.println(bean1 == bean2);
+        System.out.println();
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Сравнение бинов Cat");
+        System.out.println("Сылка на первый бин = " + cat1);
+        System.out.println("Сылка на второй бин = " + cat2);
+        System.out.println(cat1 == cat2);
     }
 }
